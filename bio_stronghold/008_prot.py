ad43cd4fbe3a3158738fee02d30885b3b93d59e7
ikumen <ikumen@gnoht.com>
2016-02-18 22:04:03 -0800

    Trying to trigger initial travis build

---
+++ bio_stronghold/008_prot.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+
+"""
+008_prot.py: Translating RNA into Protein (http://rosalind.info/problems/prot/)
+
+Problem: The 20 commonly occurring amino acids are abbreviated by using 20 letters 
+from the English alphabet (all letters except for B, J, O, U, X, and Z). Protein 
+strings are constructed from these 20 symbols. Henceforth, the term genetic string 
+will incorporate protein strings along with DNA strings and RNA strings.
+
+The RNA codon table dictates the details regarding the encoding of specific codons 
+into the amino acid alphabet.
+
+
+Given: An RNA string ss corresponding to a strand of mRNA (of length at most 10 kbp).
+Return: The protein string encoded by ss.
+
+Sample Dataset:
+AUGGCCAUGGCGCCCAGAACUGAGAUCAAUAGUACCCGUAUUAACGGGUGA
+
+Sample Output:
+MAMAPRTEINSTRING
+"""
+import sys
+import pytest
+
+
+#def tranlate_to_protein(rna):
+
+
+#def test_translate_to_protein():
+
+
+def main():
+	'''Main runner, to read data, compute and saves output.'''
+	# rediret stdout to our output file
+	#sys.stdout = open('output/.txt', 'w')
+
+	# with open('data/rosalind_ini.txt') as input:
+	# 	s = input.readline()		
+
+
+	# with open('output/.txt', 'w') as output:
+	# 	output.write()
+
+
+if __name__ == '__main__':
+	main()
